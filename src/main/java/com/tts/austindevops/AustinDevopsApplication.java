ef8980b99bedbed6775c114b7077759ccba82f86
Lionel Beato <lionel.beato@gmail.com>
2021-03-11 09:45:07 -0500

    first commit

---
+++ src/main/java/com/tts/austindevops/AustinDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.tts.austindevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AustinDevopsApplication.class, args);
+	}
+
+}
