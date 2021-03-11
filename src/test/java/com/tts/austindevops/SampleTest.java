8f744fc2cd429589690478e5f14336129ecb7257
Lionel Beato <lionel.beato@gmail.com>
2021-03-11 10:12:51 -0500

    adds failing test

---
+++ src/test/java/com/tts/austindevops/SampleTest.java
@@ -0,0 +1,16 @@
+package com.tts.austindevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Test
+    public void test() {
+        fail("This test is not implemented!");
+    }
+
+}
